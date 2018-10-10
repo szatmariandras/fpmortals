f06a12db2a39cc8b847d23a5a28f70a84707cd72
strati <strati@strati.hu>
2018-10-10 21:21:37 +0200

    scalafmt, OAuth, jsonformat

--- build.sbt
+++ build.sbt
@@ -17,11 +17,22 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "com.github.mpilquist"  %% "simulacrum"  % "0.13.0",
-  "com.chuusai"           %% "shapeless"   % "2.3.3",
-  "org.scalaz"            %% "scalaz-core" % "7.2.26",
-  "com.propensive"        %% "contextual"  % "1.1.0",
-  "org.scalatest"         %% "scalatest"   % "3.0.5" % "test"
+  "com.github.mpilquist"  %% "simulacrum"     % "0.13.0",
+  "com.chuusai"           %% "shapeless"      % "2.3.3",
+  "org.scalaz"            %% "scalaz-core"    % "7.2.26",
+  "com.propensive"        %% "contextual"     % "1.1.0",
+  "eu.timepit"            %% "refined-scalaz" % "0.9.2",
+  "org.scalatest"         %% "scalatest"      % "3.0.5" % "test"
+)
+
+val derivingVersion = "1.0.0"
+libraryDependencies ++= Seq(
+  "org.scalaz" %% "deriving-macro" % derivingVersion % "provided",
+  compilerPlugin("org.scalaz" %% "deriving-plugin" % derivingVersion),
+  "org.scalaz" %% "scalaz-deriving"            % derivingVersion,
+  "org.scalaz" %% "scalaz-deriving-magnolia"   % derivingVersion,
+  "org.scalaz" %% "scalaz-deriving-scalacheck" % derivingVersion,
+  "org.scalaz" %% "scalaz-deriving-jsonformat" % derivingVersion
 )
 
 addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.7")
---
+++ src/main/scala/hu/andrasszatmari/fpmortals/OAuth/Messages.scala
@@ -0,0 +1,65 @@
+package hu.andrasszatmari.fpmortals.OAuth
+
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.string.Url
+
+import jsonformat._, JsDecoder.ops._
+
+final case class AuthRequest(
+  redirect_uri: String Refined Url,
+  scope: String,
+  client_id: String,
+  prompt: String = "consent",
+  response_type: String = "code",
+  access_type: String = "offline"
+)
+
+final case class AccessRequest(
+  code: String,
+  redirect_uri: String Refined Url,
+  client_id: String,
+  client_secret: String,
+  scope: String = "",
+  grant_type: String = "authorization_code"
+)
+
+final case class AccessResponse(
+  access_token: String,
+  token_type: String,
+  expires_in: Long,
+  refresh_token: String
+)
+
+final case class RefreshRequest(
+  client_secret: String,
+  refresh_token: String,
+  client_id: String,
+  grant_type: String = "refresh_token"
+)
+
+final case class RefreshResponse(
+  access_token: String,
+  token_type: String,
+  expires_in: Long
+)
+
+object AccessResponse {
+  implicit val json: JsDecoder[AccessResponse] = JsDecoder.obj(4) { j =>
+    for {
+      acc <- j.getAs[String]("access_token")
+      tpe <- j.getAs[String]("token_type")
+      exp <- j.getAs[Long]("expires_in")
+      ref <- j.getAs[String]("refresh_token")
+    } yield AccessResponse(acc, tpe, exp, ref)
+  }
+}
+
+object RefreshResponse {
+  implicit val json: JsDecoder[RefreshResponse] = JsDecoder.obj(3) { j =>
+    for {
+      acc <- j.getAs[String]("access_token")
+      tpe <- j.getAs[String](key="token_type")
+      exp <- j.getAs[Long]("expires_in")
+    } yield RefreshResponse(acc, tpe, exp)
+  }
+}
